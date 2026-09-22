175c326d2e52674fe7538b5e0f1b279db5925ca7
jakubpolak1-boop <jakubpolik1@gmail.com>
2026-09-22 10:47:48 +0200

    Update cvicenie1.sql

--- cvicenie1.sql
+++ cvicenie1.sql
@@ -37,4 +37,17 @@ SELECT * FROM products;
 SELECT * FROM orders;
 
 
-
+----------------------------------------------------------------------------------------------------------------------------------------
+SELECT 
+    orders.order_id, 
+    customers.customer_name, 
+    orders.sales
+FROM 
+    orders
+JOIN 
+    customers ON orders.customer_id = customers.customer_id
+WHERE 
+    orders.sales > 500
+ORDER BY 
+    orders.sales DESC; 
+-----------------------------------------------------------------------------------------------
